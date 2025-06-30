63e16caa89895807984efef14ecda7c21a8806e0
Mark Quach <markquach@student.unsw.edu.au>
2025-06-30 16:44:50 +1000

    Backend routes

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,15 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from routes.auth_routes import auth_routes
+from routes.message_routes import message_routes
+from routes.group_routes import group_routes
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
-def home():
-    return jsonify({"message": "Welcome to the Warm and Fuzzies API!"})
+app.register_blueprint(auth_routes)
+app.register_blueprint(message_routes)
+app.register_blueprint(group_routes)
 
 if __name__ == '__main__':
     app.run(debug=True)
